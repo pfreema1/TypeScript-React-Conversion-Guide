e3a34a6378c221c4aa6030c732e317b45e42100c
Paul <pfreema1@gmail.com>
2018-01-28 10:48:30 -0500

    changed gameStateBar to ts

--- TicTacToe_JS/webpack.config.js
+++ TicTacToe_JS/webpack.config.js
@@ -1,18 +1,25 @@
 module.exports = {
+  // change to .tsx if necessary
   entry: './src/app.jsx',
   output: {
     filename: './dist/bundle.js'
   },
   resolve: {
-    extensions: [".js", ".jsx"]
+    // changed from extensions: [".js", ".jsx"]
+    extensions: [".ts", ".tsx", ".js", ".jsx"]
   },
   module: {
     rules: [
-      { test: /\.jsx?$/, use: { loader: 'babel-loader' } }
+      // changed from { test: /\.jsx?$/, use: { loader: 'babel-loader' } },
+      { test: /\.(t|j)sx?$/, use: { loader: 'awesome-typescript-loader' } },
+      // addition - add source-map support 
+      { enforce: "pre", test: /\.js$/, loader: "source-map-loader" }
     ]
   },
   externals: {
     "react": "React",
     "react-dom": "ReactDOM",
   },
+  // addition - add source-map support
+  devtool: "source-map"
 }
